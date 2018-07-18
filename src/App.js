7b6e99ce81eb2cb3d0d96c5948b79eadb81f91d9
hsskey <slingshot800@gmail.com>
2018-07-18 22:29:41 +0900

    this.setState() 기능추가

--- src/App.js
+++ src/App.js
@@ -3,25 +3,6 @@ import logo from './logo.svg';
 import './App.css';
 import Movie from './Movie';
 
-const movies = [
-  {
-
-  "title": "blackPanther",
-  "poster": "http://t1.daumcdn.net/liveboard/movie/84bbece8ba5340e4924170534e22a0ae.JPG"
-  }, 
-  {
-  "title": "infinityWar",
-  "poster": "https://images-na.ssl-images-amazon.com/images/I/A1t8xCe9jwL._SY679_.jpg"
-  },
-  {
-  "title": "deadPool",
-  "poster": "http://www.filmfeeder.co.uk/wp-content/uploads/2018/05/deadpool2body1.png"
-  },
-  {
-  "title": "antman&wasp",
-  "poster": "http://digitalspyuk.cdnds.net/18/18/768x384/landscape-1525180279-ant-man.jpg"
-  }
-]
 
 
 
@@ -32,13 +13,40 @@ class App extends Component {
     //Update 예시 : componentWillUpdate()시 로딩아이콘 , componentdidUpdate()시 로딩아이콘 hide
   
   state = {
-    greeting: "New Moive"
+    greeting: "New Moive",
+    movies : [
+      {
+    
+      "title": "blackPanther",
+      "poster": "http://t1.daumcdn.net/liveboard/movie/84bbece8ba5340e4924170534e22a0ae.JPG"
+      }, 
+      {
+      "title": "infinityWar",
+      "poster": "https://images-na.ssl-images-amazon.com/images/I/A1t8xCe9jwL._SY679_.jpg"
+      },
+      {
+      "title": "deadPool",
+      "poster": "http://www.filmfeeder.co.uk/wp-content/uploads/2018/05/deadpool2body1.png"
+      },
+      {
+      "title": "antman&wasp",
+      "poster": "http://digitalspyuk.cdnds.net/18/18/768x384/landscape-1525180279-ant-man.jpg"
+      }
+    ]
   }
 
   componentDidMount(){
     setTimeout(()=>{
       this.setState({
-        greeting:"New Moive is comming"
+        //영화목록을 추가하고싶을때 아래와같이 추가
+        movies: [
+          ...this.state.movies,
+          {
+            "title":"captainMarvel",
+            "poster":"https://i.redd.it/ac02e53cn5v01.jpg"
+          },
+        ]
+        
       })
     },5000)
   }
@@ -46,7 +54,7 @@ class App extends Component {
   render() {
     return <div className="App">
         {this.state.greeting}
-        {movies.map((movie,index) => {
+        {this.state.movies.map((movie,index) => {
           return <Movie title={movie.title} poster={movie.poster} key={index}/>;
         })}
       </div>;
