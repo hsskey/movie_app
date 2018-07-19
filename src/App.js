9d41d77efbb94d7910715254960fee97d6f73139
hsskey <slingshot800@gmail.com>
2018-07-19 13:52:38 +0900

    async await 변경

--- src/App.js
+++ src/App.js
@@ -15,19 +15,31 @@ class App extends Component {
   state = {};
 
   componentDidMount(){
-    fetch('https://yts.am/api/v2/list_movies.json?sort_by=download_count')
-    .then(response =>response.json())
-    .then(json => console.log(json))
-    .catch(err =>console.log(err))
-  }
-  //영화 list를 불러오는 function
-  _renderMovies = () =>{
-    const movies =this.state.movies.map((movie,index) => {
-       return <Movie title={movie.title} poster={movie.poster} key={index}/>;
-    })
-    return movies;
+    this._getMovies()
   }
 
+  _getMovies = async () => {
+    const movies = await this._callApi()
+    this.setState({
+      movies
+    })
+  }
+  //배열 형태로 return
+  _callApi = () =>{
+    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=download_count')
+    .then(response =>response.json())
+    .then(json => json.data.movies)
+    .catch(err =>console.log(err))
+  }
+  //실제 출력부
+  _renderMovies = () =>{
+    const movies = this.state.movies.map(movie =>{
+      return <Movie title={movie.title} poster={movie.large_cover_image} key={movie.id}/>
+    })
+    return movies
+  }
+
+  //loading state
   render() {
     return <div className="App">
         {this.state.movies?this._renderMovies() : 'Loading'}
